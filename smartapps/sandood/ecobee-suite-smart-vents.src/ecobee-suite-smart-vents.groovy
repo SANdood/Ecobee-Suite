6caafc26120f7d29d5daa073c152f4d993795dcb
Barry Burke <SANdood@users.noreply.github.com>
2018-02-11 13:11:24 -0500

    v1.4.0- Change Devices' & Mgr's Name

--- smartapps/sandood/ecobee-suite-smart-vents.src/ecobee-suite-smart-vents.groovy
+++ smartapps/sandood/ecobee-suite-smart-vents.src/ecobee-suite-smart-vents.groovy
@@ -24,8 +24,9 @@
  *  1.2.0 - Sync version number with new holdHours/holdAction support
  *	1.2.1 - Protect against LOG type errors
  *	1.3.0 - Major Release: renamed and moved to "sandood" namespace
+ *	1.4.0 - Renamed parent Ecobee Suite Manager
  */
-def getVersionNum() { return "1.3.0" }
+def getVersionNum() { return "1.4.0" }
 private def getVersionLabel() { return "Ecobee Suite Smart Vents, version ${getVersionNum()}" }
 import groovy.json.JsonSlurper
 
@@ -35,7 +36,7 @@ definition(
 	author: "Barry A. Burke (storageanarchy at gmail dot com)",
 	description: "Automates SmartThings-controlled vents to meet a target temperature in a room",
 	category: "Convenience",
-	parent: "sandood:Ecobee Suite (Connect)",
+	parent: "sandood:Ecobee Suite Manager",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/ecobee.png",
 	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/ecobee@2x.png",
 	singleInstance: false
