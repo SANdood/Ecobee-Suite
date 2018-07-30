a0eb180605742c10b682be991033de214bfa2479
Barry Burke <SANdood@users.noreply.github.com>
2018-07-30 12:38:08 -0400

    v1.6.10 - Resync for revised reservation system

--- smartapps/sandood/ecobee-suite-smart-zones.src/ecobee-suite-smart-zones.groovy
+++ smartapps/sandood/ecobee-suite-smart-zones.src/ecobee-suite-smart-zones.groovy
@@ -24,9 +24,10 @@
  *	1.5.00 - Release number synchronization
  *	1.5.01 - Allow Ecobee Suite Thermostats only
  *	1.5.02 - Converted all math to BigDecimal
- *	1.6.00- Release number synchronization
+ *	1.6.00 - Release number synchronization
+ *	1.6.10- Resync for parent-based reservations
  */
-def getVersionNum() { return "1.6.00" }
+def getVersionNum() { return "1.6.10" }
 private def getVersionLabel() { return "Ecobee Suite Smart Zones Helper, version ${getVersionNum()}" }
 
 definition(
