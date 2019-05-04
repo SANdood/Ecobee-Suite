4640243f90d71847fc587208dd388a0af85f56dc
Barry Burke <SANdood@users.noreply.github.com>
2019-05-04 14:55:47 -0400

    1.7.00d: Formatting changes

--- smartapps/sandood/ecobee-suite-routines.src/ecobee-suite-routines.groovy
+++ smartapps/sandood/ecobee-suite-routines.src/ecobee-suite-routines.groovy
@@ -30,8 +30,8 @@
  *	1.6.13 - Use 'fanAuto' if fanMinutes is explicitly set to 0
  *	1.7.00 - Universal support for both SmartThings and Hubitat
  */
-def getVersionNum() { return "1.7.00a" }
-private def getVersionLabel() { return "Ecobee Suite Mode${isST?'/Routine':''}/Program Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
+def getVersionNum() { return "1.7.00d" }
+private def getVersionLabel() { return "Ecobee Suite Mode${isST?'/Routine':''}/Program Helper,\nversion ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
 	name: "ecobee Suite Routines",
@@ -687,7 +687,7 @@ private Boolean getIsHEHub() { return atomicState.isHE }					// if (isHEHub) ...
 
 private def getParentSetting(String settingName) {
 	// def ST = (atomicState?.isST != null) ? atomicState?.isST : isST
-	log.debug "isST: ${isST}, isHE: ${isHE}"
+	//log.debug "isST: ${isST}, isHE: ${isHE}"
 	return isST ? parent?.settings?."${settingName}" : parent?."${settingName}"	
 }
 //
