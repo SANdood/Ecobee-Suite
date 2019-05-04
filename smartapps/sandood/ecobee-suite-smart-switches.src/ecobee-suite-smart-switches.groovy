8bdd599bc8992c887d6359fc0ef51e6b7f7a115e
Barry Burke <SANdood@users.noreply.github.com>
2019-05-04 15:02:31 -0400

    1.7.00d: Formatting changes

--- smartapps/sandood/ecobee-suite-smart-switches.src/ecobee-suite-smart-switches.groovy
+++ smartapps/sandood/ecobee-suite-smart-switches.src/ecobee-suite-smart-switches.groovy
@@ -26,8 +26,8 @@
  *	1.6.10 - Resync for parent-based reservations
  *	1.7.00 - Universal code supports both SmartThings and Hubitat
  */
-def getVersionNum() { return "1.7.00a" }
-private def getVersionLabel() { return "Ecobee Suite Smart Switch/Dimmer/Vent Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
+def getVersionNum() { return "1.7.00d" }
+private def getVersionLabel() { return "Ecobee Suite Smart Switch/Dimmer/Vent Helper,\nversion ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
 	name: "ecobee Suite Smart Switches",
@@ -249,7 +249,7 @@ private Boolean getIsHEHub() { return atomicState.isHE }					// if (isHEHub) ...
 
 private def getParentSetting(String settingName) {
 	// def ST = (atomicState?.isST != null) ? atomicState?.isST : isST
-	log.debug "isST: ${isST}, isHE: ${isHE}"
+	//log.debug "isST: ${isST}, isHE: ${isHE}"
 	return isST ? parent?.settings?."${settingName}" : parent?."${settingName}"	
 }
 //
