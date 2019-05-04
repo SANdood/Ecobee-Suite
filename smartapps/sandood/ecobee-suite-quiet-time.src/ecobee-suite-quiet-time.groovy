483a64e22b0bb20e0d51c7566c2da1e8de8af059
Barry Burke <SANdood@users.noreply.github.com>
2019-05-04 14:54:24 -0400

    1.7.00d: Formatting updates

--- smartapps/sandood/ecobee-suite-quiet-time.src/ecobee-suite-quiet-time.groovy
+++ smartapps/sandood/ecobee-suite-quiet-time.src/ecobee-suite-quiet-time.groovy
@@ -31,8 +31,8 @@
  *	1.6.15 - Added scheduled Auto Off for Quiet Time
  *	1.7.00 - Universal support for both SmartThings and Hubitat
  */
-def getVersionNum() { return "1.7.00a" }
-private def getVersionLabel() { return "Ecobee Suite Quiet Time Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
+def getVersionNum() { return "1.7.00d" }
+private def getVersionLabel() { return "Ecobee Suite Quiet Time Helper,\nversion ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
 	name: "ecobee Suite Quiet Time",
@@ -652,7 +652,7 @@ private Boolean getIsHEHub() { return atomicState.isHE }					// if (isHEHub) ...
 
 private def getParentSetting(String settingName) {
 	// def ST = (atomicState?.isST != null) ? atomicState?.isST : isST
-	log.debug "isST: ${isST}, isHE: ${isHE}"
+	//log.debug "isST: ${isST}, isHE: ${isHE}"
 	return isST ? parent?.settings?."${settingName}" : parent?."${settingName}"	
 }
 //
