cad02da6422ea873cb5e53159c3989619b179825
Barry Burke <SANdood@users.noreply.github.com>
2019-05-04 15:00:12 -0400

    1.7.00d: Formatting changes

--- smartapps/sandood/ecobee-suite-smart-room.src/ecobee-suite-smart-room.groovy
+++ smartapps/sandood/ecobee-suite-smart-room.src/ecobee-suite-smart-room.groovy
@@ -26,8 +26,8 @@
  *	1.6.12 - Added support for "generic" vents (dimmers), as with Smart Vents
  *	1.7.00 - Universal code now supports both SmartThings and Hubitat
  */
-def getVersionNum() { return "1.7.00a" }
-private def getVersionLabel() { return "Ecobee Suite Smart Room Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
+def getVersionNum() { return "1.7.00d" }
+private def getVersionLabel() { return "Ecobee Suite Smart Room Helper,\nversion ${getVersionNum()} on ${getHubPlatform()}" }
 import groovy.json.JsonSlurper
 
 definition(
@@ -575,7 +575,7 @@ private Boolean getIsHEHub() { return atomicState.isHE }					// if (isHEHub) ...
 
 private def getParentSetting(String settingName) {
 	// def ST = (atomicState?.isST != null) ? atomicState?.isST : isST
-	log.debug "isST: ${isST}, isHE: ${isHE}"
+	//log.debug "isST: ${isST}, isHE: ${isHE}"
 	return isST ? parent?.settings?."${settingName}" : parent?."${settingName}"	
 }
 //
