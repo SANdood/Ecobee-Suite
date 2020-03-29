5e5516f98c7cbc38d66c1aec45faa269bce14f62
Barry Burke <SANdood@users.noreply.github.com>
2020-03-29 18:52:35 -0400

    1.8.07- Optimizations & Enhancements

--- smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
+++ smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
@@ -2,7 +2,7 @@
  *	Ecobee Suite Smart Mode
  *
  *	Copyright 2018-2020 Justin Leonard, Barry A. Burke
- *
+ * 
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
  *
