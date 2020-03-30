445eb92eb16b4f7954b3033f244b1afbe3ee9ee2
Barry Burke <SANdood@users.noreply.github.com>
2020-03-30 10:22:42 -0400

    1.8.09- Fixed appDisplayName in sendMessage

--- smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
+++ smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
@@ -39,11 +39,12 @@
  *	1.8.06 - No longer LOGs to parent (too much overhead for too little value)
  *	1.8.07 - New SHPL, using Global Fields instead of atomicState
  *	1.8.08 - Fixed pageLabel issue of CustomNotifications page
+ *	1.8.09 - Fixed appDisplayName in sendMessage
  */
 import groovy.json.*
 import groovy.transform.Field
 
-String getVersionNum()		{ return "1.8.08" }
+String getVersionNum()		{ return "1.8.09" }
 String getVersionLabel()	{ return "Ecobee Suite Smart Mode, Programs & Setpoints Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
@@ -1820,9 +1821,9 @@ void sendMessage(notificationMessage) {
                         if((echo.size() > 1) && echoDeviceObjs && echoDeviceObjs?.size()) {
                             //NOTE: Only sends command to first device in the list | We send the list of devices to announce one and then Amazon does all the processing
                             def devJson = new groovy.json.JsonOutput().toJson(echoDeviceObjs)
-                            echo[0].sendAnnouncementToDevices(msg, (msgPrefix?:(app.displayName?:(app.label?:app.name))), echoDeviceObjs)	// , changeVol, restoreVol) }
+                            echo[0].sendAnnouncementToDevices(msg, (msgPrefix?:atomicState.appDisplayName), echoDeviceObjs)	// , changeVol, restoreVol) }
                         } else if (echo.size() == 1) {
-                            echo.playAnnouncement(msg, (msgPrefix?:(app.displayName?:(app.label?:app.name))))
+                            echo.playAnnouncement(msg, (msgPrefix?:atomicState.appDisplayName))
                         } else {
                         	notEcho*.deviceNotification(msg)
                         }
@@ -1882,9 +1883,9 @@ void sendMessage(notificationMessage) {
                         if((echo.size() > 1) && echoDeviceObjs && echoDeviceObjs?.size()) {
                             //NOTE: Only sends command to first device in the list | We send the list of devices to announce one and then Amazon does all the processing
                             def devJson = new groovy.json.JsonOutput().toJson(echoDeviceObjs)
-                            echo[0].sendAnnouncementToDevices(msg, (msgPrefix?:(app.displayName?:(app.label?:app.name))), echoDeviceObjs)	// , changeVol, restoreVol) }
+                            echo[0].sendAnnouncementToDevices(msg, (msgPrefix?:atomicState.appDisplayName), echoDeviceObjs)	// , changeVol, restoreVol) }
                         } else if (echo.size() == 1) {
-                            echo.playAnnouncement(msg, (msgPrefix?:(app.displayName?:(app.label?:app.name))))
+                            echo.playAnnouncement(msg, (msgPrefix?:atomicState.appDisplayName))
                         } else {
                         	notEcho*.deviceNotification(msg)
                         }
