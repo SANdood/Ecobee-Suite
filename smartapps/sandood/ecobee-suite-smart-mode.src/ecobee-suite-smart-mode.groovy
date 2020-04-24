b908ac02202f27b95d62c6e0e4268808d09a4d01
Barry Burke <SANdood@users.noreply.github.com>
2020-04-24 14:28:29 -0400

    1.8.19- Miscellaneous updates & fixes

--- smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
+++ smartapps/sandood/ecobee-suite-smart-mode.src/ecobee-suite-smart-mode.groovy
@@ -32,11 +32,12 @@
  *	1.8.16 - Updated formatting; added Do Not Disturb Modes & Time window
  *	1.8.17 - HOTFIX: location.mode subscription; sendHoldHours in setThermostatProgram()
  *	1.8.18 - HOTFIX: updated sendNotifications() for latest Echo Speaks Device version 3.6.2.0
+ *	1.8.19 - Miscellaneous updates & fixes
  */
 import groovy.json.*
 import groovy.transform.Field
 
-String getVersionNum()		{ return "1.8.18" }
+String getVersionNum()		{ return "1.8.19" }
 String getVersionLabel()	{ return "Ecobee Suite Smart Mode, Programs & Setpoints Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
@@ -481,13 +482,19 @@ def mainPage() {
 			input(name: "infoOff",		title: inputTitle("Disable info logging"),		type: "bool", required: false, defaultValue: false, submitOnChange: true, width: 3)
 		}		
 		// Standard footer
-		if (ST) {
-			section(getVersionLabel().replace('er, v',"er\nV")+"\n\nCopyright \u00a9 2017-2020 Barry A. Burke\nAll rights reserved.\n\nhttps://github.com/SANdood/Ecobee-Suite") {}
-		} else {
-			section() {
-				paragraph(getFormat("line")+"<div style='color:#5BBD76;text-align:center'>${getVersionLabel()}<br><small>Copyright \u00a9 2017-2020 Barry A. Burke - All rights reserved.</small><br>"+
-						  "<a href='https://github.com/SANdood/Ecobee-Suite' target='_blank' style='color:#5BBD76'><u>Click here for the Ecobee Suite GitHub Repository</u></a></div>")
-			}
+        if (ST) {
+            section("") {
+        		href(name: "hrefNotRequired", description: "Tap to donate via PayPal", required: false, style: "external", image: "https://raw.githubusercontent.com/SANdood/Icons/master/Ecobee/paypal-green.png",
+                	 url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MJQD5NGVHYENY&currency_code=USD&source=url", title: "Your donation is appreciated!" )
+    		}
+        	section(getVersionLabel().replace('er, v',"er\nV")+"\n\nCopyright \u00a9 2017-2020 Barry A. Burke\nAll rights reserved.") {}
+        } else {
+        	section() {
+        		paragraph(getFormat("line")+"<div style='color:#5BBD76;text-align:center'>${getVersionLabel()}<br><small>Copyright \u00a9 2017-2020 Barry A. Burke - All rights reserved.</small><br>"+
+						  "<small><i>Your</i>&nbsp;</small><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MJQD5NGVHYENY&currency_code=USD&source=url' target='_blank'>" + 
+						  "<img src='https://raw.githubusercontent.com/SANdood/Icons/master/Ecobee/paypal-green.png' border='0' width='64' alt='PayPal Logo' title='Please consider donating via PayPal!'></a>" +
+						  "<small><i>donation is appreciated!</i></small></div>" )
+            }
 		}
 	}
 }
