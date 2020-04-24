97814bf365d64ebc76141ecf726d5c8ff824e995
Barry Burke <SANdood@users.noreply.github.com>
2020-04-24 14:30:17 -0400

    1.8.08- Miscellaneous updates & fixes

--- smartapps/sandood/ecobee-suite-smart-switches.src/ecobee-suite-smart-switches.groovy
+++ smartapps/sandood/ecobee-suite-smart-switches.src/ecobee-suite-smart-switches.groovy
@@ -21,10 +21,11 @@
  *	1.8.05 - Allow individual un-pause from peers, even if was already paused
  *	1.8.06 - Updated formatting; added Do Not Disturb Modes & Time window
  *	1.8.07 - Fixed reverseActions()
+ *	1.8.08 - Miscellaneous updates & fixes
  */
 import groovy.transform.Field
 
-String getVersionNum()		{ return "1.8.07" }
+String getVersionNum()		{ return "1.8.08" }
 String getVersionLabel() 	{ return "Ecobee Suite Smart Switch/Dimmer/Vent Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
@@ -171,11 +172,17 @@ def mainPage() {
 		}  
 		// Standard footer
         if (ST) {
-        	section(getVersionLabel().replace('er, v',"er\nV")+"\n\nCopyright \u00a9 2017-2020 Barry A. Burke\nAll rights reserved.\n\nhttps://github.com/SANdood/Ecobee-Suite") {}
+            section("") {
+        		href(name: "hrefNotRequired", description: "Tap to donate via PayPal", required: false, style: "external", image: "https://raw.githubusercontent.com/SANdood/Icons/master/Ecobee/paypal-green.png",
+                	 url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MJQD5NGVHYENY&currency_code=USD&source=url", title: "Your donation is appreciated!" )
+    		}
+        	section(getVersionLabel().replace('er, v',"er\nV")+"\n\nCopyright \u00a9 2017-2020 Barry A. Burke\nAll rights reserved.") {}
         } else {
         	section() {
         		paragraph(getFormat("line")+"<div style='color:#5BBD76;text-align:center'>${getVersionLabel()}<br><small>Copyright \u00a9 2017-2020 Barry A. Burke - All rights reserved.</small><br>"+
-                		  "<a href='https://github.com/SANdood/Ecobee-Suite' target='_blank' style='color:#5BBD76'><u>Click here for the Ecobee Suite GitHub Repository</u></a></div>")
+						  "<small><i>Your</i>&nbsp;</small><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MJQD5NGVHYENY&currency_code=USD&source=url' target='_blank'>" + 
+						  "<img src='https://raw.githubusercontent.com/SANdood/Icons/master/Ecobee/paypal-green.png' border='0' width='64' alt='PayPal Logo' title='Please consider donating via PayPal!'></a>" +
+						  "<small><i>donation is appreciated!</i></small></div>" )
             }
 		}
     }
