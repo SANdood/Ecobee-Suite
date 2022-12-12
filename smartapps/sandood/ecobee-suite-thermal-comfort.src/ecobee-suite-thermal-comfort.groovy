388021f1e967457846b26bdacc1755cc540c4f87
Barry Burke <SANdood@users.noreply.github.com>
2022-12-12 13:20:34 -0500

    1.8.20- Fix for Hubitat 'supportedThermostatModes', etc.

--- smartapps/sandood/ecobee-suite-thermal-comfort.src/ecobee-suite-thermal-comfort.groovy
+++ smartapps/sandood/ecobee-suite-thermal-comfort.src/ecobee-suite-thermal-comfort.groovy
@@ -33,11 +33,12 @@
  *	1.8.17 - Fix getThermostatPrograms()
  *	1.8.18 - Fix getThermostatModes()
  *	1.8.19 - Fix sendMessage() for new Samsung SmartThings app
+ *	1.8.20 - Fix for Hubitat 'supportedThermostatModes', etc.
  */
 import groovy.json.*
 import groovy.transform.Field
 
-String getVersionNum()		{ return "1.8.19" }
+String getVersionNum()		{ return "1.8.20" }
 String getVersionLabel() 	{ return "Ecobee Suite Thermal Comfort Helper, version ${getVersionNum()} on ${getHubPlatform()}" }
 
 definition(
@@ -518,14 +519,15 @@ List getThermostatPrograms() {
 }
 List getThermostatModes() {
 	List statModes = ["off","heat","cool","auto","auxHeatOnly"]
+	List tm = []
     if (settings.theThermostat) {
         if (HE) {
-    	    def tm = theThermostat.currentValue('supportedThermostatModes')
-            if (tm && (tm != '[]')) statModes = tm[1..-2].tokenize(", ")
+    	    tm = new JsonSlurper().parseText(theThermostat.currentValue('supportedThermostatModes'), true)
         } else {
-            statModes = new JsonSlurper().parseText(theThermostat.currentValue('supportedThermostatModes'))
+            tm = new JsonSlurper().parseText(theThermostat.currentValue('supportedThermostatModes'))
         }
     }
+	if (tm != []) statModes = tm
     return statModes.sort(false)
 }
 def initialize() {
